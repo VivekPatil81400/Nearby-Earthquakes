c4015caf439b1e4d14688677643c9ec1aef414c8
VivekPatil81400 <59966933+VivekPatil81400@users.noreply.github.com>
2022-05-04 06:22:39 +0200

    final

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from operator import le
 import requests, json
+import haversine as hs
 
 
 api_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
@@ -14,4 +15,22 @@ for eq_dict in all_data:
     title.append(eq_dict['properties']['title'])
     id.append(eq_dict['id'])
 
-#print earthquake data closer to user location
+lat1 = float(input("Enter latitude: "))
+lon1 = float(input("Enter longitude: "))
+loc1 = (lat1,lon1)
+locs = []
+for i in range(len(lons)):
+    locs.append((lats[i], lons[i]))
+
+dis = []
+for loc2 in locs:
+    dis.append(hs.haversine(loc1,loc2))
+
+dis = [int(x) for x in dis]
+    
+dis, title = zip(*sorted(zip(dis, title)))
+
+for i in range(0,9):
+    print((title[i]) + " || "+ str(dis[i]))
+
+
